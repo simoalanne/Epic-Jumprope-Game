1b4acda34d7aaa870a6af08115520a99221fa1d7
simoalanne <144778803+simoalanne@users.noreply.github.com>
2024-05-29 10:49:44 +0300

    flashbang

---
+++ Assets/Code/Flashbang.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flashbang : MonoBehaviour
+{
+    [SerializeField] private AudioClip _flashbangSound;
+    [SerializeField] private float _flashDuration = 2f;
+    private float _alphaValue = 1f;
+    private RawImage _flashImage;
+
+    public void Start()
+    {
+        _flashImage = GetComponentInChildren<RawImage>();
+        _flashImage.color = new Color(1, 1, 1, _alphaValue);
+        StartCoroutine(FlashAnim());
+    }
+
+    IEnumerator FlashAnim()
+    {
+        yield return new WaitForSeconds(0.5f);
+        float startTime = Time.time;
+        float endTime = startTime + _flashDuration;
+        while (Time.time < endTime)
+        {
+            float elapsed = Time.time - startTime;
+            float normalizedTime = elapsed / _flashDuration;
+            _alphaValue = Mathf.Exp(-normalizedTime * 2.5f);
+            _flashImage.color = new Color(1, 1, 1, _alphaValue);
+            yield return null;
+        }
+
+        _flashImage.color = new Color(1, 1, 1, 0);
+        Destroy(gameObject);
+    }
+}
