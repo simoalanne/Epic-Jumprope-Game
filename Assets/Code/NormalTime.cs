0e8959f55858904f6817288e5a79c5f6bb4e8262
Veikka Luukkonen <61163039+Vegetius-s@users.noreply.github.com>
2024-05-29 12:33:24 +0300

    Jotain voidboxii yms

---
+++ Assets/Code/NormalTime.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalTime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
+}
